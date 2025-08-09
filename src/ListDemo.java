7496422643ebba98b02181d12b245a329111c744
VMz69 <114504815+VMz69@users.noreply.github.com>
2025-08-09 17:28:28 -0600

    Create ListDemo.java

---
+++ src/ListDemo.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDemo {
+    public static void main(String[] args) {
+        List<String> lista = new ArrayList<>();
+        lista.add("Elemento 1");
+        lista.add("Elemento 2");
+        System.out.println("Lista: " + lista);
+    }
+}
