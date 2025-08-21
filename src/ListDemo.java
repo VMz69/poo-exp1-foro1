0aaef28ac5de79cf6eabd6595f8d6159ef763284
Nelson <nelson14maury@gmail.com>
2025-08-20 18:04:11 -0600

    List_updated

--- src/ListDemo.java
+++ src/ListDemo.java
@@ -8,10 +8,11 @@ public class ListDemo {
         lista.add("Elemento 2");
         System.out.println("Lista: " + lista);
 
-        String removido = lista.removeFirst();
-        System.out.println("Se eliminó (por índice): " + removido);
-        System.out.println("Lista tras remover por índice: " + lista);
 
+        String removido = lista.removeFirst();
+        System.out.println("Se eliminó (por índice 0): " + removido);
+        System.out.println("Lista tras remover por índice: " + lista);
     }
 }
+
 
